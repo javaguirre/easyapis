5669a22f5b0d66be4aef34f6209b6d12b0628d5f
Javier Aguirre <contacto@javaguirre.net>
2012-04-25 16:26:34 +0200

    Now calls is a ruby module

--- calls.rb
+++ calls.rb
@@ -1,24 +1,28 @@
 require "httparty"
 require "nokogiri"
 
-def phone_codes
-    response = HTTParty.get("http://countrycode.org/")
+module Calls
+    class << self
+        def phone_codes
+            response = HTTParty.get("http://countrycode.org/")
 
-    doc = Nokogiri::HTML(response.body)
+            doc = Nokogiri::HTML(response.body)
 
-    table = doc.xpath("//table[@id='main_table_blue']").first
-    tbody = table.children[1]
-    phone_codes = Hash.new
+            table = doc.xpath("//table[@id='main_table_blue']").first
+            tbody = table.children[1]
+            phone_codes = Hash.new
 
-    tbody.children.each do |item|
-        code = item.children[2].content.chop.split(" / ")[0]
-        phone_code = item.children[4].content.chop
-        phone_codes[code] = phone_code unless not code or phone_code.empty? or code.empty?
+            tbody.children.each do |item|
+                code = item.children[2].content.chop.split(" / ")[0]
+                phone_code = item.children[4].content.chop
+                phone_codes[code] = phone_code unless not code or phone_code.empty? or code.empty?
+            end
+
+            #File.open("phone_codes.js", "w") do |f1|
+            #    phone_codes = phone_codes.to_json
+            #    f1.puts "var phone_codes = #{phone_codes}"
+            #end
+            phone_codes
+        end
     end
-
-    #File.open("phone_codes.js", "w") do |f1|
-    #    phone_codes = phone_codes.to_json
-    #    f1.puts "var phone_codes = #{phone_codes}"
-    #end
-    phone_codes
 end
